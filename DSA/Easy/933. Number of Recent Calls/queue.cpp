d755d1bb9271ecdd3b1a5fe850de817e6e62227a
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2025-05-23 21:24:35 +0530

    Create queue.cpp

---
+++ DSA/Easy/933. Number of Recent Calls/queue.cpp
@@ -0,0 +1,18 @@
+class RecentCounter {
+    queue<int> counter;
+public:
+    RecentCounter() {}
+    int ping(int t) {
+        counter.push(t);
+        while(counter.front() < t - 3000){
+            counter.pop();
+        }
+        return counter.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
